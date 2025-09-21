b807a46d2730836190d1277e4038f66a2223cead
Siddharth Mourya <mouryasiddharth98@gmail.com>
2025-09-21 07:11:23 +0000

    scds

--- src/server.ts
+++ src/server.ts
@@ -13,17 +13,18 @@ async function startServer() {
         console.log('✅ Database connected successfully');
 
         // Start server
-        const server = app.listen(env.PORT, () => {
-            console.log('Server listen callback', server.address());
-            const actualPort = (server.address() as any).port; // ensures correct port
-            console.log(`🚀 Server running at http://localhost:${actualPort} in ${env.NODE_ENV} mode`);
+        app.listen(env.PORT, () => {
+            console.log(`🚀 Server running at http://localhost:${env.PORT} in ${env.NODE_ENV} mode`);
+            // console.log('Server listen callback', server.address());
+            // const actualPort = (server.address() as any).port; // ensures correct port
+            // console.log(`🚀 Server running at http://localhost:${actualPort} in ${env.NODE_ENV} mode`);
         });
 
         // Optional: handle server errors
-        server.on('error', (err: any) => {
-            console.error('❌ Server failed to start:', err);
-            process.exit(1);
-        });
+        // server.on('error', (err: any) => {
+        //     console.error('❌ Server failed to start:', err);
+        //     process.exit(1);
+        // });
     } catch (err) {
         console.error('❌ Failed to start server:', err);
         process.exit(1);
