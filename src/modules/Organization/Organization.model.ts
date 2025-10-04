b9e23f539620e60a0bf2efff9439ec5cc33a7539
Siddharth Mourya <mouryasiddharth98@gmail.com>
2025-10-04 15:07:59 +0530

    test

--- src/modules/Organization/Organization.model.ts
+++ src/modules/Organization/Organization.model.ts
@@ -1,26 +1,26 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
 export interface IOrganization extends Document {
-    name: string;
-    address: string;
-    orgEmail: string;
-    orgPhone: string;
-    isActive?: boolean;
-    cin?: string;
-    createdAt: Date;
-    updatedAt: Date;
+  name: string;
+  address: string;
+  orgEmail: string;
+  orgPhone: string;
+  isActive?: boolean;
+  cin?: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 const OrganizationSchema = new Schema<IOrganization>(
-    {
-        name: { type: String, required: true, unique: true },
-        address: { type: String },
-        orgEmail: { type: String },
-        orgPhone: { type: String },
-        isActive: { type: Boolean, default: true },
-        cin: { type: String },
-    },
-    { timestamps: true },
+  {
+    name: { type: String, required: true },
+    address: { type: String, required: true },
+    orgEmail: { type: String, required: true, unique: true },
+    orgPhone: { type: String, required: true, unique: true },
+    isActive: { type: Boolean, default: true },
+    cin: { type: String },
+  },
+  { timestamps: true },
 );
 
 export const Organization = mongoose.model<IOrganization>('Organization', OrganizationSchema);
