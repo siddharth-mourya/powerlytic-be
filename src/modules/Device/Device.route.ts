985a8ba3956044cfa6ac5d6fd052fee143c9dc34
Siddharth Mourya <mouryasiddharth98@gmail.com>
2025-10-05 14:51:34 +0530

    w

--- src/modules/Device/Device.route.ts
+++ src/modules/Device/Device.route.ts
@@ -14,7 +14,7 @@ const router = Router();
 router.post('/', authMiddleware, requireRole(UserRoles.CompanyAdmin), createDevice);
 router.get('/', authMiddleware, getDevices);
 router.get('/:id', authMiddleware, getDeviceById);
-router.put('/:id', authMiddleware, requireRole(UserRoles.OrgAdmin), updateDevice);
+router.put('/:id', authMiddleware, requireRole(UserRoles.CompanyAdmin), updateDevice);
 router.delete('/:id', authMiddleware, requireRole(UserRoles.CompanyAdmin), deleteDevice);
 
 export default router;
